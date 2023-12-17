a18c5deb54c9c9117b72e0ff0b0e4d600a803812
joshuakalungi <joshuakalungi101@gmail.com>
2023-12-17 22:30:35 +0300

    improved date and time

--- quote-generator/index.js
+++ quote-generator/index.js
@@ -2,13 +2,31 @@
 // 2. Every time the button gets pressed, a random whole number needs to be generated.
 // 3. The number will be used as a representation of the array index number for the quote array.
 // 4. Once we retrieve the randomly picked quote from the array using our randomly generated whole number, we will place it to the HTML document.
-function startTime() {
+
+
+// Time
+let startTime =() => {
     const currentDate = new Date();
     let hours =currentDate.getHours();
     let minutes = currentDate.getMinutes();
     let seconds = currentDate.getSeconds();
     var ampm = "";
-    minutes = checkTime(minutes);  //adds a zero to minutes less than 10
+
+    //adds a zero to seconds less than 10
+    const checkSeconds =(seconds) => {
+    if (seconds <10){seconds=`0${seconds}`};
+    return seconds;
+    }
+
+
+    //adds a zero to minutes less than 10
+    const checkTime = (minutes) => {
+    if (minutes<10) {minutes = `0${minutes}`};  // add zero in front of numbers < 10
+    return minutes;
+    }
+
+
+    minutes = checkTime(minutes);  
     seconds = checkSeconds(seconds);
 
     if (hours > 12) {
@@ -28,23 +46,15 @@ function startTime() {
   }
 
     document.getElementById('display').innerHTML = hours+":"+minutes+":"+seconds+ampm;
-    let update = setTimeout(function(){startTime()},500);
+    let update = setTimeout(()=> {startTime()},500);
 }
 
-const checkTime = (minutes) => {
-    if (minutes<10) {minutes = `0${minutes}`};  // add zero in front of numbers < 10
-    return minutes;
-}
 
-const checkSeconds =(seconds) => {
-    if (seconds <10){seconds=`0${seconds}`};
-    return seconds;
-}
 // //date
-// function startDate() {
-//   var d = new Date();
-//   var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-//   document.getElementById("date").innerHTML = days[d.getDay()]+" | "+[d.getMonth()+1]+"/"+d.getDate()+"/"+d.getFullYear();
-// }
+const startDate =() => {
+    const currentDate = new Date();
+    const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    document.getElementById("date").innerHTML = days[currentDate.getDay()]+" | "+[currentDate.getMonth()+1]+"/"+currentDate.getDate()+"/"+currentDate.getFullYear();
+}
 
 
