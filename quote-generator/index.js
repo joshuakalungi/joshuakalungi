413ed01a8b23f7493f749231fed6f0d2acf65f25
joshuakalungi <joshuakalungi101@gmail.com>
2023-12-17 22:09:06 +0300

    modified startdate() time()

--- quote-generator/index.js
+++ quote-generator/index.js
@@ -2,44 +2,55 @@
 // 2. Every time the button gets pressed, a random whole number needs to be generated.
 // 3. The number will be used as a representation of the array index number for the quote array.
 // 4. Once we retrieve the randomly picked quote from the array using our randomly generated whole number, we will place it to the HTML document.
-function startTime() {
-    var today=new Date();
-    var h=today.getHours();
-    var m=today.getMinutes();
-    var s=today.getSeconds();
-    var ampm = "";
-    m = checkTime(m);
 
-    if (h > 12) {
-    	h = h - 12;
+
+//current date declaration
+const currentDate = new Date();
+    
+
+const startTime = () => {
+    let hours =currentDate.getHours();
+    let minutes = currentDate.getMinutes();
+    let seconds = currentDate.getSeconds();
+    var ampm = "";
+    minutes = checkTime(minutes);  //adds a zero to minutes less than 10
+    seconds = checkSeconds(seconds);
+
+    if (hours > 12) {
+    	hours = hours - 12;
     	ampm = " PM";
-    } else if (h == 12){
-        h = 12;
+    } else if (hours == 12){
+        hours = 12;
     	ampm = " AM";
-    } else if (h < 12){
+    } else if (hours < 12){
         ampm = " AM";
     } else {
         ampm = "PM";
     };
   
-  if(h==0) {
+  if(hours==0) {
     h=12;
   }
 
-document.getElementById('display').innerHTML = h+":"+m+ampm;
-    var t = setTimeout(function(){startTime()},500);
+    document.getElementById('display').innerHTML = hours+":"+minutes+":"+seconds+ampm;
+    let update = setTimeout(function(){startTime()},500);
 }
 
-function checkTime(i) {
-    if (i<10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
+const checkTime = (minutes) => {
+    if (minutes<10) {minutes= `0${minutes}`};  // add zero in front of numbers < 10
+    return minutes;
 }
 
-//date
-function startDate() {
-  var d = new Date();
-  var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-  document.getElementById("date").innerHTML = days[d.getDay()]+" | "+[d.getMonth()+1]+"/"+d.getDate()+"/"+d.getFullYear();
+const checkSeconds =(seconds) => {
+    if (seconds <10){seconds=`0${seconds}`};
+    return seconds;
+}
+
+// //date
+const startDate = () => {
+   const currentDate = new Date();
+//   var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+//   document.getElementById("date").innerHTML = days[d.getDay()]+" | "+[d.getMonth()+1]+"/"+d.getDate()+"/"+d.getFullYear();
 }
 
 
