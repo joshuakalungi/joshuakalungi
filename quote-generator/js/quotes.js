eecac60e3d59e17038a9fcd3b7aabd00f7b34dad
joshuakalungi <joshuakalungi101@gmail.com>
2023-12-28 23:21:12 +0300

    tweet button and new quote button

--- quote-generator/js/quotes.js
+++ quote-generator/js/quotes.js
@@ -6,12 +6,13 @@ const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const author = document.getElementById('author');
 const quoteBtn = document.getElementById('new-quote');
+const twitterBtn = document.getElementById('twitter-button');
 
 function newQuote() {
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-
+    // passing text content
     quoteText.textContent = quote.text;
-
+    // passing author content
     author.textContent = quote.author;
 } 
 
@@ -41,6 +42,17 @@ async function getQuotes() {
         alert(error);
     }
 }
+
+
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${author.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+quoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // calling the getRequest function onLoad
 getQuotes();
