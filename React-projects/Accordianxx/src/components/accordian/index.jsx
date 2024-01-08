44dacf7dfeada99d42e0d4f8d57fb9061dbe6ff9
joshuakalungi <joshuakalungi101@gmail.com>
2024-01-08 19:12:10 +0300

    pulled data to index.js

--- React-projects/Accordianxx/src/components/accordian/index.jsx
+++ React-projects/Accordianxx/src/components/accordian/index.jsx
@@ -1,5 +1,25 @@
+import { useState } from "react"
+import data from "./data.js"
+
 export default function Accordian() {
-    return <div>
-        Accordian
-    </div>
+    const [selected, setSelected] = useState("");
+
+
+    return ( 
+        <div className="wrapper">
+            <div className="accordian">
+                {
+                    data.map((dataItem)=> {
+                        return (
+                            <div className="item">
+                                <div className="title">
+                                    <h3>{ dataItem.question }</h3>
+                                    <span>+</span>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>)
 }
