ed110d6a78b8f2a04d68d6283639e4527af7afc1
joshuakalungi <joshuakalungi101@gmail.com>
2024-01-12 01:20:42 +0300

    api test was successful

--- React-projects/imageSlider/src/components/imageSlider/index.jsx
+++ React-projects/imageSlider/src/components/imageSlider/index.jsx
@@ -1,4 +1,49 @@
-export default function ImageSlider(){
+import { useEffect } from "react"
+import { useState } from "react"
+
+// eslint-disable-next-line react/prop-types
+export default function ImageSlider({url,page=1, limit=5 }){
+    const [images, setImages] = useState([])
+    const [slider,setSlider] = useState(0)
+    const [errorMsg, setErrorMsg] = useState(null)
+    const [loading,setLoading] = useState(false)
+
+
+    async function fetchImages(getUrl){
+        // catching errors
+        try {
+            setLoading(true)
+
+            const response = await fetch(`${getUrl}?page=${page}&limit=${limit}`)
+
+            const data = await response.json()
+
+            if(data) {
+                setImages(data)
+                setLoading(false)
+            }
+
+        } catch (error) {
+            setErrorMsg(error.message)
+            setLoading(false)
+        }
+    }
+
+
+    useEffect(() => {
+        if(url !== "") fetchImages(url)
+    },[url])
+
+    console.log(images)
+
+    if(loading){
+        return <div>Loading Please wait...</div>
+    }
+
+    if(errorMsg !== null){
+        return <div>Error occured! {errorMsg}</div>
+    }
+
     return (
         <div className="container">Image Slider</div>
     )
