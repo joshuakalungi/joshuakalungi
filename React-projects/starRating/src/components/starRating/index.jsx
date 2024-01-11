c51acb9caba70bbe267310cf6038ce911922c246
joshuakalungi <joshuakalungi101@gmail.com>
2024-01-11 06:47:48 +0300

    added the dynamic functionality

--- React-projects/starRating/src/components/starRating/index.jsx
+++ React-projects/starRating/src/components/starRating/index.jsx
@@ -1,5 +1,42 @@
-export default function StarRating(){
+import { FaStar } from 'react-icons/fa'
+import { useState } from 'react'
+
+
+// eslint-disable-next-line react/prop-types
+export default function StarRating({ noOfStars = 5 }){
+
+    const [rating, setRating] = useState(0);
+    const [hover, setHover] = useState(0);
+
+    function handleClick(getCurrentIndex){
+        setRating(getCurrentIndex)
+    }
+    
+    function handleMouseMove(getCurrentIndex){
+        setHover(getCurrentIndex)
+    }
+
+    function handleMouseLeave(){
+        setHover(rating)
+    }
+
     return (
-        <div>StarRating</div>
+        <div className="starRating">
+            {
+                [...Array(noOfStars)].map((_,index) => {
+
+                    index +=1
+
+                    return ( <FaStar
+                        key={index}
+                        className={index <= hover || rating ? 'active' : 'inactive'}
+                        onClick={()=> handleClick(index)}
+                        onMouseMove={()=> handleMouseMove(index)}
+                        onMouseLeave={()=> handleMouseLeave()}
+                    />
+                )
+             })
+            }
+        </div>
     )
 }
