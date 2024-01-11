a7923acbd4243f43c6702cb86d3450ed6683684a
joshuakalungi <joshuakalungi101@gmail.com>
2024-01-11 04:22:46 +0300

    added the hex functionality

--- React-projects/RandomColors/src/components/randomColors/index.jsx
+++ React-projects/RandomColors/src/components/randomColors/index.jsx
@@ -1,21 +1,61 @@
+import { useState } from "react";
+
 export default function RandomColor() {
+    const [typeOfColor, setTypeOfColor] = useState("hex"); //default type of color is hex
+    const [color, setColor] = useState("#ffffff"); //default color is white
+
+    function randomColorUtility(length){
+        return Math.floor(Math.random() *length);
+    }
+    
+    function handleCreateRandomHexColor () {
+        const hex = [ 1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+
+        let hexColor = '#';
+
+        for(let i = 0; i <6;i++){
+            hexColor += hex[randomColorUtility(hex.length)]
+        }
+        console.log(hexColor);
+
+        setColor(hexColor);
+    }
+
+    function handleCreateRandomRgbColor(){
+        // const r = 
+
+    }
 
     return (
-        <div className="wrapper">
-            <button style={{ 
+        <>
+        <div style={{
+            width: '100vw',
+            height: '100vh',
+            background: color,
+            position: 'relative',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+        }} className="wrapper">
+            <button onClick={() =>setTypeOfColor('hex')} style={{ 
                 marginRight: '20px' 
             }} >Create Hex Color</button>
             
-            <button style= {{
+            <button onClick={()=>setTypeOfColor('rgb')} style= {{
                 marginRight: '20px'
             }}>Create rgb Color</button>
             
-            <button>Create Random Color</button>
+            <button onClick={typeOfColor === 'hex' ? handleCreateRandomHexColor : handleCreateRandomRgbColor}>Create Random Color</button>
 
-            <div style={{
+            
+        </div>
+        <div style={{
                 marginTop: '40px',
                 fontWeight: 'bold',
-            }}>Output</div>
-        </div>
+                position: 'absolute',
+                bottom: '35%',
+                left: '50%',
+        }}>Output</div>
+        </>
     )
 }
